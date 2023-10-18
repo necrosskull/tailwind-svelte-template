76071244b45013bbf29771408154107f6ec597b4
Slava Kuryaev <92830706+necrosskull@users.noreply.github.com>
2023-10-18 21:36:34 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# tailwind-svelte-template
+ 
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<div class="min-h-[calc(100dvh)] justify-center items-center flex flex-col">
+    <h1 class="text-6xl text-center text-amber-500 font-bold underline">
+    Tailwind in Svelte
+  </h1>
+</div>
